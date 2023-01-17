f3f892b3ea00c3c3e2b0c9c98bf8f518fa4716a9
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-17 17:45:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "01 Jan 2024"
+const newYears = "30 Jun 2023"
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
